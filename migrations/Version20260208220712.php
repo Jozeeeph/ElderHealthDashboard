be3c07f2aa3f35a4742daaaa2d9bb36bc049fbd7
L4tifs <latifagar4@gmail.com>
2026-02-08 23:33:39 +0100

    fotter+partial

--- migrations/Version20260208220712.php
+++ migrations/Version20260208220712.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260208190413 extends AbstractMigration
+final class Version20260208220712 extends AbstractMigration
 {
     public function getDescription(): string
     {
