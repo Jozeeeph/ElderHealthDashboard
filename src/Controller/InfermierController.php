f2e096a67dff099a90e342a85d3c356f30bc2857
mimimaryem <sghirimaryem37@gmail.com>
2026-02-10 19:12:07 +0100

    update client

--- src/Controller/InfermierController.php
+++ src/Controller/InfermierController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Repository\ConsultationRepository;
+use App\Repository\RendezVousRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,11 +14,37 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_PERSONNEL_MEDICAL')]
 final class InfermierController extends AbstractController
 {
-    #[Route('/infermier', name: 'app_infermier_interface')]
-    public function index(): Response
+    private function requirePersonnel(): Utilisateur
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        if (!$user instanceof Utilisateur) {
+            throw $this->createAccessDeniedException('Acces refuse.');
+        }
+        if (strtoupper((string) $user->getRoleMetier()) !== 'PERSONNEL_MEDICAL') {
+            throw $this->createAccessDeniedException('Acces reserve au personnel medical.');
+        }
+
+        return $user;
+    }
+
+    #[Route('/infermier', name: 'app_infermier_interface')]
+    public function index(
+        ConsultationRepository $consultationRepository,
+        RendezVousRepository $rendezVousRepository
+    ): Response
+    {
+        $user = $this->requirePersonnel();
+        $consultationCount = $consultationRepository->count(['personnelMedical' => $user]);
+        $plannedRdvCount = $rendezVousRepository->countPlannedForPersonnel($user);
+        $plannedRendezVous = $rendezVousRepository->findPlannedForPersonnel($user, 5);
+
         return $this->render('FrontOffice/infermier/index.html.twig', [
             'controller_name' => 'InfermierController',
+            'nurseName' => $user->getPrenom(),
+            'consultationCount' => $consultationCount,
+            'plannedRdvCount' => $plannedRdvCount,
+            'plannedRendezVous' => $plannedRendezVous,
         ]);
     }
 }
