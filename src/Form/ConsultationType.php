4359a8fbf9808e5be7ec93ac50887999c5246786
mimimaryem <sghirimaryem37@gmail.com>
2026-02-15 16:18:30 +0100

    pré-bilan

--- src/Form/ConsultationType.php
+++ src/Form/ConsultationType.php
@@ -9,6 +9,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -35,6 +37,20 @@ class ConsultationType extends AbstractType
                 'widget' => 'single_text',
             ])
             ->add('lieu', TextType::class)
+            ->add('poidsKg', NumberType::class, [
+                'required' => false,
+                'scale' => 2,
+                'html5' => true,
+                'attr' => ['step' => '0.1', 'min' => 20, 'max' => 300],
+            ])
+            ->add('tensionSystolique', IntegerType::class, [
+                'required' => false,
+                'attr' => ['min' => 60, 'max' => 250],
+            ])
+            ->add('tensionDiastolique', IntegerType::class, [
+                'required' => false,
+                'attr' => ['min' => 30, 'max' => 150],
+            ])
             ->add('patient', EntityType::class, [
                 'class' => Utilisateur::class,
                 'choice_label' => function (Utilisateur $u) {
