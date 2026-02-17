86edaba58a056eaf467dbde3ab62f0dff39f03d8
mimimaryem <sghirimaryem37@gmail.com>
2026-02-18 00:31:01 +0100

    Resolve

--- migrations/Version20260217233016.php
+++ migrations/Version20260217233016.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260216133751 extends AbstractMigration
+final class Version20260217233016 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20260216133751 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event ADD reminder_sent TINYINT NOT NULL');
         $this->addSql('ALTER TABLE rendez_vous ADD is_paid TINYINT DEFAULT 0 NOT NULL, ADD paid_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event DROP reminder_sent');
         $this->addSql('ALTER TABLE rendez_vous DROP is_paid, DROP paid_at');
     }
 }
